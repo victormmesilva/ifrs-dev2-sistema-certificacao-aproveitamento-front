0edd21623503315b34bd0af4a154b0414bd4dfb7
Victor Silva <victormmsilva97@gmail.com>
2019-09-23 00:14:42 -0300

    add react-input

--- src/components/CertificacaoConhecimentosForm.jsx
+++ src/components/CertificacaoConhecimentosForm.jsx
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Select from 'react-select';
+import Files from 'react-files';
+import Blob from 'blob'
+/* import FormData from 'form-data' */
+import './inputFiles.css';
 
+const defaultSelect = {label: '...', value: ''};
 const cursosJSON = [
     {id: 1, nome: 'Análise e Desenvolvimento de Sistemas'},
     {id: 2, nome: 'Letras'},
@@ -18,10 +24,8 @@ function CertificacaoConhecimentosForm(){
     const [curso, setCurso] = useState('');
     const [discCursadaAntes, setDiscCursadaAntes] = useState('');
     const [discSolicitada, setDiscSolicitada] = useState('');
-    /* const [anexos, setAnexo] = useState([]); */
+    const [anexos, setAnexos] = useState([]);
     
-    /* 195128 */
-
     const fazerRequisicao = (event) => {
         event.preventDefault();
         const requisicao = {
@@ -35,8 +39,47 @@ function CertificacaoConhecimentosForm(){
     }
 
     useEffect(() => {
-        setDisciplinas(disciplinasJSON);
+        /* console.log(discSolicitada); */
+        if(curso === ''){
+            setDiscSolicitada('');
+        } else {
+            setDisciplinas(disciplinasJSON);
+        }    
     }, [curso]);
+
+    useEffect(() => {console.log(anexos)}, [anexos]);
+
+    const onFilesChange = (files) => {
+        /* setAnexos([...anexos, files].flat()); */
+        setAnexos(files);
+    }
+    
+    const onFilesError = (error, file) => {
+        console.log('error code ' + error.code + ': ' + error.message)
+    }
+    
+    const filesRemoveOne = (file) => {
+        /* refs.files.removeFile(file) */
+    }
+    
+    const removerTodosAnexos = (event) => {
+        event.preventDefault();
+        /* setAnexos([]); */
+        console.log(anexos)
+        anexos.files.removeFiles();
+    }
+    
+    const filesUpload = () => {
+        /*const formData = new FormData()
+        Object.keys(state.files).forEach((key) => {
+            const file = state.files[key]
+            formData.append(key, new Blob([file], { type: file.type }), file.name || 'file')
+        })
+
+          axios.post(`/files`, formData)
+        .then(response => window.alert(`${state.files.length} files uploaded succesfully!`))
+        .catch(err => window.alert('Error uploading files :(')) */
+    }
 
     return(
         <>
@@ -44,26 +87,25 @@ function CertificacaoConhecimentosForm(){
 
             <div className="form-group">
                 <label htmlFor="curso" className="mb-1">Curso</label>
-                <select id="curso"
-                        className="form-control"
-                        onChange={({target}) => setCurso(target.value)}
-                >
-                    <option value=''>...</option>
-                    {cursos.map(curso => <option key={curso.id} value={curso.id}>{curso.nome}</option>)}
-                </select>
+                <Select
+                    id="curso"
+                    onChange={(option) => setCurso(option)}
+                    selectedOption={null}
+                    options={cursos.map(curso => ({ value: curso.id, label: curso.nome}))}
+                    defaultValue={defaultSelect}
+                />
             </div>
             
             <div className="form-group">
                 <label htmlFor="disciplina-solicitada" className="mb-1">Disciplina solicitada</label>
-                <select id="disciplina-solicitada"
-                        className="form-control"
-                        onChange={({target}) => setDiscSolicitada(target.value)}
-                        disabled={!curso}
-                >
-                    <option value=''>...</option>
-                    {disciplinas.map(disciplina => <option key={disciplina.id} value={disciplina.id}>{disciplina.nome}</option>)}
-                    
-                </select>
+                <Select
+                    id="disciplina-solicitada"
+                    onChange={(option) => setCurso(option)}
+                    selectedOption={null}
+                    options={disciplinas.map(disciplina => ({ value: disciplina.id, label: disciplina.nome}))}
+                    defaultValue={defaultSelect}
+                    isDisabled={!curso}
+                />
             </div>
             
             <div className="form-group">
@@ -71,18 +113,55 @@ function CertificacaoConhecimentosForm(){
                 <input type="text" className="form-control" id="disciplina-anterior" onChange={({target}) => setDiscCursadaAntes(target.value)}/>
             </div>
             
-            {/* <div className="form-group">
-                <label htmlFor="anexos" className="mb-1">Anexos</label>
-                <input type="file" className="form-control" id="anexos" multiple/>
-            </div> */}
-
-            {/* <div class="box">
-                <input type="file" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple=""/>
-                <label htmlFor="file-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> 
-                    <span>Selecione um arquivo…</span>
-                    </label>
-            </div> */}
+            <div className="form-group">
+                <Files
+                    ref={anexos}
+                    className='files-dropzone-list'
+                    style={{ height: '100px' }}
+                    onChange={onFilesChange}
+                    onError={onFilesError}
+                    multiple
+                    maxFiles={10}
+                    maxFileSize={10000000}
+                    minFileSize={0}
+                    clickable
+                >
+                    {'Solte aqui os arquivos para anexar'}
+                </Files>
+                {
+                    anexos.length > 0 ? 
+                    <div className='files-list'>
+                            <ul>
+                                {anexos.map((file) =>
+                                    <li className='files-list-item' key={file.id}>
+                                        <div className='files-list-item-preview'>{
+                                            file.preview.type === 'image' ? 
+                                            <img className='files-list-item-preview-image' src={file.preview.url} />
+                                            : 
+                                            <div className='files-list-item-preview-extension'>{file.extension}</div>
+                                        }</div>
+                                        
+                                        <div className='files-list-item-content'>
+                                            <div className='files-list-item-content-item files-list-item-content-item-1'>{file.name}</div>
+                                            <div className='files-list-item-content-item files-list-item-content-item-2'>{file.sizeReadable}</div>
+                                        </div>
+                                        
+                                        <div
+                                            id={file.id}
+                                            className='files-list-item-remove'
+                                            /* onClick={filesRemoveOne.bind(this, file)} // eslint-disable-line */
+                                        />
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    : null
+                }
+            </div> 
+            <div className="d-flex justify-content-start">
+                <button className="btn btn-link m-1" onClick={removerTodosAnexos}>Remover todos os anexos</button>
+                {/*<button onClick={filesUpload}>Upload</button> */}
+            </div>
             
             <div className="d-flex justify-content-end">
                 <button type="reset" className="btn btn-link m-1">Limpar</button>
